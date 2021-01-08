0aebb7893eef779c38fa44bf5db82819b2393fd2
DanieBrown <dabrown101292@gmail.com>
2021-01-07 22:01:31 -0500

    init commit 2

---
+++ src/main/java/com/blog/dabnbackapi/DabnbackApiApplication.java
@@ -0,0 +1,13 @@
+package com.blog.dabnbackapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DabnbackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DabnbackApiApplication.class, args);
+	}
+
+}
